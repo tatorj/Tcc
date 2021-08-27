e53841253099ccec8cb9e1c10e941a2a25203402
tatorj <48737584+tatorj@users.noreply.github.com>
2021-08-27 12:52:08 -0300

    Add files via upload

---
+++ Comparing/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+
+
+
+using namespace std;
+
+int number_of_lines = 0;
+
+void numberoflines();
+
+int main(int argc, char* argv[]) {
+	
+	string line;
+    ifstream myfile("teste.txt");
+
+    if(myfile.is_open()){
+        while(!myfile.eof()){
+            getline(myfile,line);
+            cout<< line << endl;
+            number_of_lines++;
+        }
+        myfile.close();
+    }
+    numberoflines();
+
+}
+
+void numberoflines(){
+    number_of_lines--;
+    cout<<"number of lines in text file: " << number_of_lines << endl;
+	
+}
